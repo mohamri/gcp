a1667427f2ad633278796bd2bf9d929a5eb15eb6
moh amri <moh.amrispark@gmail.com>
2021-02-07 14:30:39 -0600

    Added the controllers

--- src/main/java/com/example/gcp/deploy/controller/ProductControler.java
+++ src/main/java/com/example/gcp/deploy/controller/ProductControler.java
@@ -1,5 +1,15 @@
 package com.example.gcp.deploy.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
+@RequestMapping("/product")
 public class ProductControler {
+
+    @GetMapping("/all")
+    public String returnAll() {
+        return "All Product";
+    }
 }
